2c9af8f60293e46fec90ac9d84ed77a25ada53ee
esmakocak <esma.kocaakk20@gmail.com>
2023-07-27 02:42:06 +0300

    Business component Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "../styles";
 
 export default function Button() {
-  return <div>Button</div>;
+  return (
+    <button className="bg-blue-gradient rounded-[10px] px-6 py-4 text-primary mt-10 font-poppins font-medium text-[18px] leading-[26px] ">
+      Get Started
+    </button>
+  );
 }
